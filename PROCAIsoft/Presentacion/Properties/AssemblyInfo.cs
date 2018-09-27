c87eef1c291e0f9f1de83341b37c49032d186c12
Glenn Lozano Tapia <Glenn Lozano Tapia@DESKTOP-LTQH1FD>
2018-09-27 18:04:30 -0500

    Acabo de crear los proyectos

--- PROCAIsoft/Presentacion/Properties/AssemblyInfo.cs
+++ PROCAIsoft/Presentacion/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("1d441084-b559-4a51-86fb-4198b5407224")]
+[assembly: Guid("86f12806-8c7d-445c-8b6a-d172e2bfba70")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
