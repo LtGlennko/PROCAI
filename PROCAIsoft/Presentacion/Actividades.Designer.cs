012ead8fdc6896fe805343a2ebdd74ee025678cb
Glenn Lozano Tapia <Glenn Lozano Tapia@DESKTOP-LTQH1FD>
2018-09-27 23:35:03 -0500

    Dia 5: La primera versión que compila, la esperanza existe

---
+++ PROCAIsoft/Presentacion/Actividades.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Presentacion
+{
+    partial class Actividades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvAct = new System.Windows.Forms.DataGridView();
+            this.btnAtras = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Actividades recientes";
+            // 
+            // dgvAct
+            // 
+            this.dgvAct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAct.Location = new System.Drawing.Point(30, 38);
+            this.dgvAct.Name = "dgvAct";
+            this.dgvAct.Size = new System.Drawing.Size(379, 205);
+            this.dgvAct.TabIndex = 1;
+            // 
+            // btnAtras
+            // 
+            this.btnAtras.Location = new System.Drawing.Point(30, 265);
+            this.btnAtras.Name = "btnAtras";
+            this.btnAtras.Size = new System.Drawing.Size(75, 23);
+            this.btnAtras.TabIndex = 2;
+            this.btnAtras.Text = "Atrás";
+            this.btnAtras.UseVisualStyleBackColor = true;
+            this.btnAtras.Click += new System.EventHandler(this.btnAtras_Click);
+            // 
+            // Actividades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 310);
+            this.Controls.Add(this.btnAtras);
+            this.Controls.Add(this.dgvAct);
+            this.Controls.Add(this.label1);
+            this.Name = "Actividades";
+            this.Text = "Actividades";
+            this.Load += new System.EventHandler(this.Actividades_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAct)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvAct;
+        private System.Windows.Forms.Button btnAtras;
+    }
+}
---
+++ PROCAIsoft/Presentacion/Actividades.cs
@@ -0,0 +1,40 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class Actividades : Form
+    {
+        private DateTime date;
+
+        public Actividades()
+        {
+            InitializeComponent();
+        }
+
+        public Actividades(BindingList<Actividad> a, DateTime d)
+        {
+            InitializeComponent();
+            date = d;
+            dgvAct.DataSource = a;
+        }
+
+        private void Actividades_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+    }
+}
