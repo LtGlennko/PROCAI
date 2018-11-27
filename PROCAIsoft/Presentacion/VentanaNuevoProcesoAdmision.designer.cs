cd6dc20edf153caf2bbcec4d36a2fe317bdc6d89
JinSSJ3 <43678736+JinSSJ3@users.noreply.github.com>
2018-11-27 17:50:06 -0500

    new design version beta
    new design version beta

--- PROCAIsoft/Presentacion/VentanaNuevoProcesoAdmision.designer.cs
+++ PROCAIsoft/Presentacion/VentanaNuevoProcesoAdmision.designer.cs
@@ -46,81 +46,89 @@
             this.label8 = new System.Windows.Forms.Label();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.gbx_ciclo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // BtnDescripcion
             // 
             this.BtnDescripcion.AutoSize = true;
-            this.BtnDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
+            this.BtnDescripcion.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnDescripcion.ForeColor = System.Drawing.Color.Teal;
             this.BtnDescripcion.Location = new System.Drawing.Point(60, 370);
             this.BtnDescripcion.Name = "BtnDescripcion";
-            this.BtnDescripcion.Size = new System.Drawing.Size(155, 17);
+            this.BtnDescripcion.Size = new System.Drawing.Size(179, 21);
             this.BtnDescripcion.TabIndex = 0;
             this.BtnDescripcion.Text = "Nombre del Proceso";
             // 
             // BtnVacantes
             // 
             this.BtnVacantes.AutoSize = true;
-            this.BtnVacantes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
+            this.BtnVacantes.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnVacantes.ForeColor = System.Drawing.Color.Teal;
             this.BtnVacantes.Location = new System.Drawing.Point(60, 316);
             this.BtnVacantes.Name = "BtnVacantes";
-            this.BtnVacantes.Size = new System.Drawing.Size(75, 17);
+            this.BtnVacantes.Size = new System.Drawing.Size(93, 21);
             this.BtnVacantes.TabIndex = 1;
             this.BtnVacantes.Text = "Vacantes";
             // 
             // lbl_fecha
             // 
             this.lbl_fecha.AutoSize = true;
-            this.lbl_fecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
+            this.lbl_fecha.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_fecha.ForeColor = System.Drawing.Color.Teal;
             this.lbl_fecha.Location = new System.Drawing.Point(60, 193);
             this.lbl_fecha.Name = "lbl_fecha";
-            this.lbl_fecha.Size = new System.Drawing.Size(52, 17);
+            this.lbl_fecha.Size = new System.Drawing.Size(62, 21);
             this.lbl_fecha.TabIndex = 2;
             this.lbl_fecha.Text = "Fecha";
             // 
             // BtnRegistrar
             // 
-            this.BtnRegistrar.Location = new System.Drawing.Point(125, 458);
+            this.BtnRegistrar.BackColor = System.Drawing.Color.White;
+            this.BtnRegistrar.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnRegistrar.Location = new System.Drawing.Point(243, 460);
             this.BtnRegistrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnRegistrar.Name = "BtnRegistrar";
-            this.BtnRegistrar.Size = new System.Drawing.Size(89, 34);
+            this.BtnRegistrar.Size = new System.Drawing.Size(114, 37);
             this.BtnRegistrar.TabIndex = 4;
             this.BtnRegistrar.Text = "Registrar";
-            this.BtnRegistrar.UseVisualStyleBackColor = true;
+            this.BtnRegistrar.UseVisualStyleBackColor = false;
             this.BtnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
             // 
             // BtnCancel
             // 
+            this.BtnCancel.BackColor = System.Drawing.Color.White;
             this.BtnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.BtnCancel.Location = new System.Drawing.Point(291, 458);
+            this.BtnCancel.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCancel.Location = new System.Drawing.Point(409, 460);
             this.BtnCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnCancel.Name = "BtnCancel";
-            this.BtnCancel.Size = new System.Drawing.Size(91, 34);
+            this.BtnCancel.Size = new System.Drawing.Size(116, 37);
             this.BtnCancel.TabIndex = 5;
             this.BtnCancel.Text = "Cancelar";
-            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.UseVisualStyleBackColor = false;
             this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
             // 
             // txt_nombre
             // 
-            this.txt_nombre.Location = new System.Drawing.Point(227, 370);
+            this.txt_nombre.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nombre.Location = new System.Drawing.Point(256, 370);
             this.txt_nombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_nombre.Name = "txt_nombre";
             this.txt_nombre.ReadOnly = true;
-            this.txt_nombre.Size = new System.Drawing.Size(235, 22);
+            this.txt_nombre.Size = new System.Drawing.Size(235, 28);
             this.txt_nombre.TabIndex = 6;
             // 
             // txt_Vacantes
             // 
-            this.txt_Vacantes.Location = new System.Drawing.Point(227, 311);
+            this.txt_Vacantes.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Vacantes.Location = new System.Drawing.Point(256, 311);
             this.txt_Vacantes.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txt_Vacantes.Name = "txt_Vacantes";
-            this.txt_Vacantes.Size = new System.Drawing.Size(55, 22);
+            this.txt_Vacantes.Size = new System.Drawing.Size(55, 28);
             this.txt_Vacantes.TabIndex = 7;
             this.txt_Vacantes.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_Vacantes_KeyPress);
             this.txt_Vacantes.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txt_Vacantes_KeyUp);
@@ -128,21 +136,22 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Teal;
             this.label1.Location = new System.Drawing.Point(60, 139);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(132, 17);
+            this.label1.Size = new System.Drawing.Size(148, 21);
             this.label1.TabIndex = 14;
             this.label1.Text = "Tipo de Proceso:";
             // 
             // cmbx_tipoProceso
             // 
+            this.cmbx_tipoProceso.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbx_tipoProceso.FormattingEnabled = true;
-            this.cmbx_tipoProceso.Location = new System.Drawing.Point(227, 139);
+            this.cmbx_tipoProceso.Location = new System.Drawing.Point(256, 139);
             this.cmbx_tipoProceso.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cmbx_tipoProceso.Name = "cmbx_tipoProceso";
-            this.cmbx_tipoProceso.Size = new System.Drawing.Size(160, 24);
+            this.cmbx_tipoProceso.Size = new System.Drawing.Size(398, 29);
             this.cmbx_tipoProceso.TabIndex = 15;
             this.cmbx_tipoProceso.SelectedValueChanged += new System.EventHandler(this.cmbx_tipoProceso_SelectedValueChanged);
             // 
@@ -158,10 +167,11 @@
             // 
             this.rbtn_1.AutoSize = true;
             this.rbtn_1.Enabled = false;
-            this.rbtn_1.Location = new System.Drawing.Point(175, 16);
+            this.rbtn_1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtn_1.Location = new System.Drawing.Point(178, 10);
             this.rbtn_1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.rbtn_1.Name = "rbtn_1";
-            this.rbtn_1.Size = new System.Drawing.Size(33, 21);
+            this.rbtn_1.Size = new System.Drawing.Size(35, 23);
             this.rbtn_1.TabIndex = 19;
             this.rbtn_1.TabStop = true;
             this.rbtn_1.Text = "I";
@@ -171,10 +181,11 @@
             // 
             this.rbtn_2.AutoSize = true;
             this.rbtn_2.Enabled = false;
-            this.rbtn_2.Location = new System.Drawing.Point(261, 16);
+            this.rbtn_2.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtn_2.Location = new System.Drawing.Point(357, 10);
             this.rbtn_2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.rbtn_2.Name = "rbtn_2";
-            this.rbtn_2.Size = new System.Drawing.Size(37, 21);
+            this.rbtn_2.Size = new System.Drawing.Size(40, 23);
             this.rbtn_2.TabIndex = 20;
             this.rbtn_2.TabStop = true;
             this.rbtn_2.Text = "II";
@@ -184,23 +195,24 @@
             // 
             this.gbx_ciclo.Controls.Add(this.rbtn_1);
             this.gbx_ciclo.Controls.Add(this.rbtn_2);
-            this.gbx_ciclo.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbx_ciclo.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.gbx_ciclo.ForeColor = System.Drawing.Color.Teal;
             this.gbx_ciclo.Location = new System.Drawing.Point(52, 242);
             this.gbx_ciclo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gbx_ciclo.Name = "gbx_ciclo";
             this.gbx_ciclo.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.gbx_ciclo.Size = new System.Drawing.Size(411, 57);
+            this.gbx_ciclo.Size = new System.Drawing.Size(573, 57);
             this.gbx_ciclo.TabIndex = 22;
             this.gbx_ciclo.TabStop = false;
             this.gbx_ciclo.Text = "Ciclo";
             // 
             // datetp_fecha
             // 
-            this.datetp_fecha.Location = new System.Drawing.Point(228, 193);
+            this.datetp_fecha.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datetp_fecha.Location = new System.Drawing.Point(257, 193);
             this.datetp_fecha.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.datetp_fecha.Name = "datetp_fecha";
-            this.datetp_fecha.Size = new System.Drawing.Size(159, 22);
+            this.datetp_fecha.Size = new System.Drawing.Size(397, 28);
             this.datetp_fecha.TabIndex = 23;
             this.datetp_fecha.ValueChanged += new System.EventHandler(this.datetp_fecha_ValueChanged);
             this.datetp_fecha.VisibleChanged += new System.EventHandler(this.datetp_fecha_VisibleChanged);
@@ -209,11 +221,11 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.Teal;
             this.label8.Location = new System.Drawing.Point(63, 529);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(46, 17);
+            this.label8.Size = new System.Drawing.Size(50, 19);
             this.label8.TabIndex = 56;
             this.label8.Text = "Atras";
             // 
@@ -232,14 +244,23 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Franklin Gothic Heavy", 16F);
-            this.label2.ForeColor = System.Drawing.Color.Teal;
-            this.label2.Location = new System.Drawing.Point(83, 49);
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(107, 36);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(370, 34);
+            this.label2.Size = new System.Drawing.Size(545, 45);
             this.label2.TabIndex = 57;
             this.label2.Text = "Nuevo Proceso de Admisión";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(797, 114);
+            this.panel1.TabIndex = 58;
             // 
             // VentanaNuevoProcesoAdmision
             // 
@@ -248,8 +269,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AliceBlue;
             this.CancelButton = this.BtnCancel;
-            this.ClientSize = new System.Drawing.Size(520, 566);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(796, 566);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.datetp_fecha);
@@ -273,6 +294,8 @@
             this.gbx_ciclo.ResumeLayout(false);
             this.gbx_ciclo.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -297,5 +320,6 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
